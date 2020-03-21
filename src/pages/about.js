8dee7f08c7ac8728b8246b7d940358e3b2f97fdd
Jonathan <jonathan.rendon@gmail.com>
2020-03-21 16:54:42 -0400

    ajustar imagen

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const About = ({ data: { about } }) => (
         <h1 className="sheet__title">{about.title}</h1>
         <p className="sheet__lead">{about.subtitle}</p>
         <div className="sheet__gallery">
-          <Img width={'50%'} fluid={about.photo.fluid} />
+          <Img fluid={about.photo.fluid} />
         </div>
         <div
           className="sheet__body"
@@ -36,7 +36,7 @@ export const query = graphql`
       title
       subtitle
       photo {
-        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+        fluid(maxWidth: 300, imgixParams: { fm: "jpg", auto: "compress" }) {
           ...GatsbyDatoCmsSizes
         }
       }
