4a42a22012a28b90875cf7768f77bbc3cca37a6c
Jonathan <jonathan.rendon@gmail.com>
2020-03-21 16:47:30 -0400

    ajustar imagen

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,7 +12,7 @@ const About = ({ data: { about } }) => (
         <h1 className="sheet__title">{about.title}</h1>
         <p className="sheet__lead">{about.subtitle}</p>
         <div className="sheet__gallery">
-          <Img fluid={about.photo.fluid} />
+          <Img width={'50%'} fluid={about.photo.fluid} />
         </div>
         <div
           className="sheet__body"
